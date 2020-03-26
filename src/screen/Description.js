cb308241301c40c72498a6b52f0f09ccd0b38740
C1290297 Edilson Jhones Batista dos Santos <c1290297@interno.bb.com.br>
2020-03-25 21:08:55 -0300

    designer tela home

--- src/screen/Description.js
+++ src/screen/Description.js
@@ -2,12 +2,37 @@ import React, { Component } from 'react';
 import { Button, View, Text, TouchableOpacity, FlatList, Alert} from 'react-native';
 
 class Description extends Component {
+    _renderItem = ({ item }) => {
+        return (
+            <View>
+                <Text>Nome {item.nome}</Text>
+            </View>
+        )
+    }
+
+    _onItemPress = (item) => {
+        if (item.indicadorCarencia == 'N') {
+            this.props.navigation.push('Description',{investimento: item})
+        }else{
+            this._showAlert()
+        }
+    }
+
     render() {
         console.log(this.props.route.params);
         const  investimento  = this.props.route.params.investimento
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>{investimento.nome}</Text>
+                <View style={{height:10, backgroundColor: '#fccb00'}}/>
+                <Text>DADOS DO INVESTIMENTO</Text>
+                <View style={{height:10, backgroundColor: '#fccb00'}}/>
+                <Text>Nome {investimento.nome}</Text>
+                <Text>Saldo total disonivel {investimento.saldoTotalDisponivel}</Text>
+                <FlatList
+                    data={investimento.acoes}
+                    renderItem={this._renderItem}
+                />
+                <Text>DADOS DO INVESTIMENTO</Text>
             </View>
         );
     }
--- src/screen/Home.js
+++ src/screen/Home.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Button, View, Text, TouchableOpacity, FlatList, Alert} from 'react-native';
+import { Button, View, Text, TouchableOpacity, FlatList, Alert, StyleSheet } from 'react-native';
 
 class Home extends Component {
 
     state = {
-        data: []
+        data: [],
+        totalInvestimento: 0
     }
 
     async componentDidMount() {
         const response = await fetch(`http://www.mocky.io/v2/5e76797e2f0000f057986099`)
         const responseJson = await response.json()
-        if (responseJson.response != undefined) {
-            this.setState({ data: responseJson.response.data.listaInvestimentos })
+        
+        if (responseJson.response.status == 200) {
+            var total = 0
+            var meusInvestimentos = responseJson.response.data.listaInvestimentos;
+            for (const index in meusInvestimentos) {
+                total = total + meusInvestimentos[index].saldoTotalDisponivel
+            }
+
+            this.setState({ data: meusInvestimentos, totalInvestimento: total })
         }
     }
 
     _renderItem = ({ item }) => {
         return (
-            <TouchableOpacity onPress={() => this._onItemPress(item)} style={{ flexDirection: 'row', padding: 10, alignItems: 'center' }}>
-                <View>
-                    <Text>{item.nome}</Text>
+            <TouchableOpacity onPress={() => this._onItemPress(item)}>
+                <View style={styles.viewItem}>
+                    <View style={{ flexDirection: 'column', }}>
+                        <Text style={{ fontWeight: 'bold', }}>{item.nome}</Text>
+                        <Text>{item.objetivo}</Text>
+                    </View>
+                    <View style={{ flexDirection: 'column', justifyContent:'center'}}>
+                        <Text style={{ fontWeight: 'bold', }}>{item.saldoTotalDisponivel}</Text>
+                    </View>
                 </View>
             </TouchableOpacity>
         )
@@ -29,32 +43,79 @@ class Home extends Component {
 
     _onItemPress = (item) => {
         if (item.indicadorCarencia == 'N') {
-            this.props.navigation.push('Description',{investimento: item})
-        }else{
+            this.props.navigation.push('Description', { investimento: item })
+        } else {
             this._showAlert()
         }
     }
 
-    _showAlert(){
+    _showAlert() {
         Alert.alert(
             'Error ',
             'O investimento ainda esta no prazo de carência, não podendo ser resgatado no momento.',
-            [{text: 'OK'},],
+            [{ text: 'OK' },],
             { cancelable: false }
         )
     }
 
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Investimento</Text>
-                <FlatList
-                    data={this.state.data}
-                    renderItem={this._renderItem}
-                />
+            <View style={styles.body}>
+                <View style={styles.hr} />
+                <View style={styles.viewHeader}>
+                    <Text style={styles.textTitle}>Investimento</Text>
+                    <Text style={styles.textDetail}>R${this.state.totalInvestimento}</Text>
+                </View>
+                <View style={styles.viewInvestimentos}>
+                    <FlatList
+                        data={this.state.data}
+                        renderItem={this._renderItem}
+                        ItemSeparatorComponent={() => <View style={{ height: 2, backgroundColor: '#f7f7f7' }} />}
+                    />
+                </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    body: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        backgroundColor: '#f7f7f7',
+    },
+    hr: {
+        backgroundColor: '#fccb00',
+        height: 5,
+    },
+    viewHeader: {
+        marginTop: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    textTitle: {
+        fontSize: 20,
+        color: '#697689',
+        marginLeft: 20,
+        fontWeight: "bold"
+    },
+    textDetail: {
+        fontSize: 20,
+        color: '#697689',
+        marginRight: 20,
+        fontWeight: "bold"
+    },
+    viewInvestimentos: {
+        marginTop: 20,
+        flexDirection: 'column',
+        backgroundColor: '#FFFFFF',
+    },
+    viewItem: { 
+        flexDirection: 'row', 
+        padding : 15,
+        justifyContent: 'space-between',
+    },
+});
 
 export default Home;
